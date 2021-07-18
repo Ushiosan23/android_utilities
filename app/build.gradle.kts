8b161c11d53e6a67edb431938d96442293a0d1c5
Ushio Okasaki <haloleyendee@outlook.com>
2021-07-17 23:33:53 -0500

    refactor(kotlin): Application test.

---
+++ app/build.gradle.kts
@@ -0,0 +1,71 @@
+plugins {
+	id("com.android.application")
+	id("kotlin-android")
+}
+
+// Extra properties
+val extra = rootProject.extra
+
+/* Android configuration */
+android {
+	// Global configurations
+	compileSdkVersion(extra["sdkVersion"] as Int)
+	buildToolsVersion(extra["buildToolsVersion"] as String)
+
+	// Default configuration
+	defaultConfig {
+		targetSdkVersion(extra["sdkVersion"] as Int)
+		minSdkVersion(extra["sdkMinVersion"] as Int)
+
+		versionCode(extra["versionCode"] as Int)
+		versionName(extra["versionName"] as String)
+
+		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+	}
+
+	// Build configurations
+	buildTypes {
+		getByName("release") {
+			isMinifyEnabled = false
+			proguardFiles(
+				getDefaultProguardFile("proguard-android-optimize.txt"),
+				file("proguard-rules.pro")
+			)
+		}
+	}
+
+	// Android features
+	buildFeatures {
+		viewBinding = true
+	}
+
+	// Jdk configuration
+	compileOptions {
+		sourceCompatibility = extra["sourceCompatibility"] as JavaVersion
+		targetCompatibility = extra["sourceCompatibility"] as JavaVersion
+	}
+
+	// Kotlin configuration
+	kotlinOptions {
+		jvmTarget = extra["jvmTarget"] as String
+	}
+}
+
+/* library dependencies */
+dependencies {
+	// Custom dependencies
+	implementation(kotlin("stdlib"))
+	implementation(project(":library"))
+
+	// Android dependencies
+	implementation("androidx.core:core-ktx:1.5.0")
+	implementation("androidx.appcompat:appcompat:1.3.0")
+	implementation("com.google.android.material:material:1.3.0")
+
+	// Jetpack dependencies
+
+	// Test implementations
+	testImplementation("junit:junit:4.13.2")
+	androidTestImplementation("androidx.test.ext:junit:1.1.2")
+	androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+}
---
+++ app/src/main/java/com/github/ushiosan23/app/activities/ActivityMain.kt
@@ -0,0 +1,82 @@
+package com.github.ushiosan23.app.activities
+
+import android.content.res.Configuration
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.content.res.AppCompatResources
+import com.github.ushiosan23.android_utilities.android.activity.ActivityCompatBinding
+import com.github.ushiosan23.android_utilities.extensions.makeSnack
+import com.github.ushiosan23.android_utilities.extensions.makeToast
+import com.github.ushiosan23.android_utilities.extensions.resolveClass
+import com.github.ushiosan23.app.R
+import com.github.ushiosan23.app.databinding.ActivityMainBinding
+import com.github.ushiosan23.app.dialogs.ExampleDialog
+import com.google.android.material.snackbar.Snackbar
+
+class ActivityMain : ActivityCompatBinding<ActivityMainBinding>() {
+
+	override val bindingClass: Class<ActivityMainBinding> = resolveClass()
+
+	override fun onActivityLoaded(savedInstanceState: Bundle?) {
+		binding.exampleButton.setOnClickListener(this::onButtonClicked)
+		binding.exampleThemeToggle.setOnClickListener(this::onToggleThemeClicked)
+		binding.exampleButtonDialog.setOnClickListener(this::onButtonDialogClicked)
+
+
+		// Change theme
+		val currentNightMode = resources
+			.configuration
+			.uiMode and Configuration.UI_MODE_NIGHT_MASK
+		val isNightModeActive = currentNightMode == Configuration.UI_MODE_NIGHT_YES
+		val drawable = AppCompatResources.getDrawable(
+			this,
+			if (isNightModeActive) R.drawable.ic_moon else R.drawable.ic_sun
+		)
+		binding.exampleThemeToggle.setImageDrawable(drawable)
+	}
+
+	/* ---------------------------------------------------------
+	 *
+	 * Events
+	 *
+	 * --------------------------------------------------------- */
+
+	private fun onButtonClicked(v: View) {
+		val snack = v.makeSnack(
+			"Hello, World!",
+			Snackbar.LENGTH_INDEFINITE
+		)
+
+		snack.setAction("Ok") {
+			snack.dismiss()
+		}.show()
+	}
+
+	@Suppress("UNUSED_PARAMETER")
+	private fun onToggleThemeClicked(v: View) {
+		// Change theme
+		val currentNightMode = resources
+			.configuration
+			.uiMode and Configuration.UI_MODE_NIGHT_MASK
+		val isNightModeActive = currentNightMode == Configuration.UI_MODE_NIGHT_YES
+
+		makeToast(
+			"Night Mode ${!isNightModeActive}",
+			Toast.LENGTH_SHORT
+		).show()
+
+
+		AppCompatDelegate.setDefaultNightMode(
+			if (isNightModeActive) AppCompatDelegate.MODE_NIGHT_NO else AppCompatDelegate.MODE_NIGHT_YES
+		)
+	}
+
+	@Suppress("UNUSED_PARAMETER")
+	private fun onButtonDialogClicked(v: View) {
+		val dialog = ExampleDialog()
+		dialog.show(supportFragmentManager, "ExampleDialog")
+	}
+
+}
