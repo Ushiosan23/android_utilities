7c7c702e7fd33dcda189f741bfd1ac6eec29fb18
Ushio Okasaki <haloleyendee@outlook.com>
2021-07-17 23:37:08 -0500

    refactor(kotlin): Random utilities was added.

--- library/src/main/java/com/github/ushiosan23/android_utilities/utilities/RandomUtils.kt
+++ library/src/main/java/com/github/ushiosan23/android_utilities/utilities/RandomUtils.kt
@@ -3,7 +3,17 @@ package com.github.ushiosan23.android_utilities.utilities
 import kotlin.math.abs
 import kotlin.random.Random
 
+/**
+ * Get system random property
+ */
 private lateinit var defaultRandom: Random
+
+/**
+ * Get system random.
+ * Initialize system random if is `null`
+ *
+ * @return Return a valid random object
+ */
 private fun getRandom(): Random = if (!::defaultRandom.isInitialized) {
 	defaultRandom = Random(getTimeMillis())
 	defaultRandom
@@ -17,9 +27,24 @@ private fun getRandom(): Random = if (!::defaultRandom.isInitialized) {
  *
  * --------------------------------------------------------- */
 
+/**
+ * Generate a random order list.
+ * This method generate a list between `0` and [total] in random order
+ *
+ * @param total Generated list size
+ * @return Returns a list with all elements in random order
+ */
 fun generateIntList(total: Int): List<Int> =
 	if (total == 0) emptyList() else generateIntListImpl(total, mutableListOf())
 
+/**
+ * Generate a random order list.
+ * This method generate a list between [start] and [end] in random order.
+ *
+ * @param start First number to start
+ * @param end Last range number
+ * @return Returns a list with all elements in random order
+ */
 fun generateIntRangeList(start: Int, end: Int): List<Int> =
 	if ((start - end) == 0) emptyList() else generateIntRangeListImpl(start until end, mutableListOf())
 
