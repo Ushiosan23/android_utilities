95d1e8d10d51c851fa92a62a1e899fe76f415b8e
Ushio Okasaki <haloleyendee@outlook.com>
2021-07-17 23:59:43 -0500

    refactor(kotlin): :arrow_up_small: Fragment Elements was added.

--- library/src/main/java/com/github/ushiosan23/android_utilities/android/fragment/FragmentBinding.kt
+++ library/src/main/java/com/github/ushiosan23/android_utilities/android/fragment/FragmentBinding.kt
@@ -8,6 +8,20 @@ import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 import com.github.ushiosan23.android_utilities.utilities.inflateBinding
 
+/**
+ * Custom fragment. Also can attach view binding layout.
+ * If you want to use this class, you need to enable android features:
+ *
+ * ```kotlin
+ * buildFeatures {
+ *   viewBinding = true
+ * }
+ * ```
+ *
+ * @param T Layout binding class
+ * @see Fragment
+ * @see IFragmentBinding
+ */
 abstract class FragmentBinding<T : ViewBinding> : Fragment(), IFragmentBinding<T> {
 
 	/* ---------------------------------------------------------
@@ -16,10 +30,16 @@ abstract class FragmentBinding<T : ViewBinding> : Fragment(), IFragmentBinding<T
 	 *
 	 * --------------------------------------------------------- */
 
+	/**
+	 * Target layout binding instance.
+	 */
 	@Suppress("MemberVisibilityCanBePrivate")
 	protected val binding: T
 		get() = internalBinding!!
 
+	/**
+	 * Property inflate fragment and attach to parent
+	 */
 	protected open val attachToRoot: Boolean = false
 
 	/* ---------------------------------------------------------
@@ -28,6 +48,10 @@ abstract class FragmentBinding<T : ViewBinding> : Fragment(), IFragmentBinding<T
 	 *
 	 * --------------------------------------------------------- */
 
+	/**
+	 * Internal binding.
+	 * This binding is used only to check binding status (because `lateinit` cause a lot of problems).
+	 */
 	private var internalBinding: T? = null
 
 	/* ---------------------------------------------------------
@@ -36,6 +60,20 @@ abstract class FragmentBinding<T : ViewBinding> : Fragment(), IFragmentBinding<T
 	 *
 	 * --------------------------------------------------------- */
 
+	/**
+	 * Called to have the fragment instantiate its user interface view.
+	 * This is optional, and non-graphical fragments can return null.
+	 * This will be called between [onCreate] and [onViewCreated].
+	 *
+	 * @param inflater The LayoutInflater object that can be used to inflate
+	 * any views in the fragment
+	 * @param container If non-null, this is the parent view that the fragment's
+	 * UI should be attached to.
+	 * @param savedInstanceState If non-null, this fragment is being re-constructed
+	 * from a previous saved state as given here.
+	 *
+	 * @return Return the view for fragment binding or `null` if the fragment contains any error
+	 */
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 		// Initialize binding
 		internalBinding = inflateBinding(
@@ -46,10 +84,19 @@ abstract class FragmentBinding<T : ViewBinding> : Fragment(), IFragmentBinding<T
 		)
 		// Check if binding is an error
 		if (internalBinding == null) return null
-		// Return view
-		onFragmentLoaded(savedInstanceState)
 		return binding.root
 	}
 
+	/**
+	 * Initialize all fragments.
+	 * This method use reflexion to inflate view binding.
+	 *
+	 * @param savedInstanceState If non-null, this fragment is being re-constructed
+	 * from a previous saved state as given here.
+	 */
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		onFragmentLoaded(savedInstanceState)
+	}
 
 }
