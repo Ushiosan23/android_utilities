8811da5323a85cad771a5eb9c42ec8041636495f
Ushio Okasaki <haloleyendee@outlook.com>
2021-07-18 00:58:57 -0500

    refactor(kotlin): :arrow_up_small: Replace specific functions to generics. And Context extensions was added.

--- library/src/main/java/com/github/ushiosan23/android_utilities/extensions/ActivityExt.kt
+++ library/src/main/java/com/github/ushiosan23/android_utilities/extensions/ActivityExt.kt
@@ -1,15 +1,10 @@
 package com.github.ushiosan23.android_utilities.extensions
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.util.DisplayMetrics
 import android.view.View
 import android.view.Window
-import android.view.WindowManager
-import android.widget.Toast
 import androidx.annotation.StringRes
 import com.google.android.material.snackbar.Snackbar
 
@@ -22,11 +17,13 @@ import com.google.android.material.snackbar.Snackbar
 /**
  * Change activity extension.
  *
+ * @param T Current activity
+ * @param L Target generic class
  * @param bundle Additional options for how the Activity should be started.
  * @param extra Extra intent arguments
  */
-inline fun <reified T> Activity.changeActivity(bundle: Bundle? = null, vararg extra: Pair<String, Any?>) =
-	with(Intent(this, T::class.java)) {
+inline fun <T : Activity, reified L> T.changeActivity(bundle: Bundle? = null, vararg extra: Pair<String, Any?>) =
+	with(Intent(this, L::class.java)) {
 		extra.forEach { putAnyExtra(it) }
 		startActivity(this, bundle)
 	}
@@ -34,16 +31,18 @@ inline fun <reified T> Activity.changeActivity(bundle: Bundle? = null, vararg ex
 /**
  * Change activity and wait for result.
  *
+ * @param T Current activity
+ * @param L Target generic class
  * @param requestCode Activity request code
  * @param bundle Additional options for how the Activity should be started.
  * @param extra Extra intent arguments
  */
-inline fun <reified T> Activity.changeActivityForResult(
+inline fun <T : Activity, reified L> T.changeActivityForResult(
 	requestCode: Int,
 	bundle: Bundle? = null,
 	vararg extra: Pair<String, Any?>
 ) =
-	with(Intent(this, T::class.java)) {
+	with(Intent(this, L::class.java)) {
 		extra.forEach { putAnyExtra(it) }
 		startActivityForResult(this, requestCode, bundle)
 	}
@@ -52,11 +51,13 @@ inline fun <reified T> Activity.changeActivityForResult(
  * Change activity extension.
  * Close current open activity after open the new activity.
  *
+ * @param T Current activity
+ * @param L Target generic class
  * @param bundle Additional options for how the Activity should be started.
  * @param extra Extra intent arguments
  */
-inline fun <reified T> Activity.changeActivityAndClose(bundle: Bundle? = null, vararg extra: Pair<String, Any?>) {
-	changeActivity<T>(bundle, *extra)
+inline fun <T : Activity, reified L> T.changeActivityAndClose(bundle: Bundle? = null, vararg extra: Pair<String, Any?>) {
+	changeActivity<T, L>(bundle, *extra)
 	finish()
 }
 
@@ -69,48 +70,24 @@ inline fun <reified T> Activity.changeActivityAndClose(bundle: Bundle? = null, v
 /**
  * Make activity snackbar. The snack is attached to activity root view.
  *
+ * @param T Current activity
  * @param resId Resource string id
  * @param duration Snack duration
  * @return Return configured snack bar
  */
-fun Activity.makeSnack(@StringRes resId: Int, duration: Int): Snackbar =
+fun <T : Activity> T.makeSnack(@StringRes resId: Int, duration: Int): Snackbar =
 	Snackbar.make(getActivityViewRoot(), resId, duration)
 
 /**
  * Make activity snackbar. The snack is attached to activity root view.
  *
+ * @param T Current activity
  * @param text Snack content text
  * @param duration Snack duration
  * @return Return configured snack bar
  */
-fun Activity.makeSnack(text: CharSequence, duration: Int): Snackbar =
+fun <T : Activity> T.makeSnack(text: CharSequence, duration: Int): Snackbar =
 	Snackbar.make(getActivityViewRoot(), text, duration)
-
-/* ---------------------------------------------------------
- *
- * Toast
- *
- * --------------------------------------------------------- */
-
-/**
- * Make activity toast.
- *
- * @param resId Toast content text
- * @param duration Toast duration
- * @return Return configured toast
- */
-fun Activity.makeToast(@StringRes resId: Int, duration: Int): Toast =
-	Toast.makeText(this, resId, duration)
-
-/**
- * Make activity toast.
- *
- * @param text Toast text content
- * @param duration Toast duration
- * @return Return configured toast
- */
-fun Activity.makeToast(text: CharSequence, duration: Int): Toast =
-	Toast.makeText(this, text, duration)
 
 /* ---------------------------------------------------------
  *
@@ -122,24 +99,9 @@ fun Activity.makeToast(text: CharSequence, duration: Int): Toast =
  * Get activity view root.
  * This view is a special element and can be found in all activities.
  *
+ * @param T Current activity
  * @return Return the activity root view
  * @see Window.ID_ANDROID_CONTENT
  */
-fun Activity.getActivityViewRoot(): View =
+fun <T : Activity> T.getActivityViewRoot(): View =
 	findViewById(Window.ID_ANDROID_CONTENT)
-
-/**
- * Get activity display metrics.
- *
- * @return Return the activity root view.
- */
-@Suppress("DEPRECATION")
-fun Activity.getDisplayMetrics(): DisplayMetrics {
-	val windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
-	// Get display depends of android version
-	val wDisplay = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) display!! else windowManager.defaultDisplay
-	// Get metrics
-	val metrics = DisplayMetrics()
-	wDisplay.getRealMetrics(metrics)
-	return metrics
-}
