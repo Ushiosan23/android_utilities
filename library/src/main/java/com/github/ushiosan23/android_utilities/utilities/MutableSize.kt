f22f72f5661943e8a524ca74139e640e1fc139fa
Ushio Okasaki <haloleyendee@outlook.com>
2021-07-17 23:33:09 -0500

    refactor(kotlin): Custom size was added.

---
+++ library/src/main/java/com/github/ushiosan23/android_utilities/utilities/MutableSize.kt
@@ -0,0 +1,15 @@
+package com.github.ushiosan23.android_utilities.utilities
+
+/**
+ * Data class used only in [com.github.ushiosan23.android_utilities.android.dialog.DialogFragmentBinding] configuration
+ */
+data class MutableSize(
+	/**
+	 * Horizontal size
+	 */
+	var width: Int,
+	/**
+	 * Vertical size
+	 */
+	var height: Int
+)
