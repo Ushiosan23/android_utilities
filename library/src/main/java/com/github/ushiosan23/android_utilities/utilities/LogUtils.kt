fcb49d13db1cbba80879d94c666a77c1fb32e17a
Ushio Okasaki <haloleyendee@outlook.com>
2021-07-17 23:32:08 -0500

    feat(kotlin): Log methods and documentation was added.

--- library/src/main/java/com/github/ushiosan23/android_utilities/utilities/LogUtils.kt
+++ library/src/main/java/com/github/ushiosan23/android_utilities/utilities/LogUtils.kt
@@ -2,6 +2,50 @@ package com.github.ushiosan23.android_utilities.utilities
 
 import android.util.Log
 
-fun logError(tag: String, throwable: Throwable) {
-	Log.e(tag, throwable.message ?: "", throwable)
-}
+/**
+ * Show log information
+ *
+ * @param tag Used to identify the source of a log message.
+ * It usually identifies the class or activity where the log call occurs.
+ * @param message The message you would like logged.
+ */
+fun logInfo(tag: String, message: CharSequence?) = Log.i(tag, message.toString())
+
+/**
+ * Show log information
+ *
+ * @param obj Used to identify the source of a log message.
+ */
+fun logInfo(obj: Any?) = logInfo(if (obj == null) "null" else obj::class.java.name, "$obj")
+
+/**
+ * Show log warning
+ *
+ * @param tag Used to identify the source of a log message.
+ * It usually identifies the class or activity where the log call occurs.
+ * @param message The message you would like logged.
+ */
+fun logWarning(tag: String, message: CharSequence?) = Log.w(tag, message.toString())
+
+/**
+ * Show log warning
+ *
+ * @param obj Used to identify the source of a log message.
+ */
+fun logWarning(obj: Any?) = logWarning(if (obj == null) "null" else obj::class.java.name, "$obj")
+
+/**
+ * Show log error
+ *
+ * @param tag Used to identify the source of a log message.
+ * It usually identifies the class or activity where the log call occurs.
+ * @param throwable The message you would like logged.
+ */
+fun logError(tag: String, throwable: Throwable) = Log.e(tag, throwable.message ?: "null", throwable)
+
+/**
+ * Show log error
+ *
+ * @param throwable Used to identify the source of a log message.
+ */
+fun logError(throwable: Throwable) = logError(throwable::class.java.name, throwable)
