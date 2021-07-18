1197b67494bdaca8781bf602ed41970b8c6accef
Ushio Okasaki <haloleyendee@outlook.com>
2021-07-17 23:46:40 -0500

    refactor(kotlin): Time utilities was added.

--- library/src/main/java/com/github/ushiosan23/android_utilities/utilities/TimeUtils.kt
+++ library/src/main/java/com/github/ushiosan23/android_utilities/utilities/TimeUtils.kt
@@ -2,8 +2,16 @@ package com.github.ushiosan23.android_utilities.utilities
 
 import java.util.*
 
+/**
+ * Get system calendar
+ */
 val systemCalendar: Calendar
 	get() = Calendar.getInstance()
 
+/**
+ * Get device time in milliseconds
+ *
+ * @return Return device UNIX time
+ */
 fun getTimeMillis(): Long =
 	systemCalendar.timeInMillis
