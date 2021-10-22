befd44929c272794048d7a2452c1b3eb55001b3b
Ushiosan23 <haloleyendee@outlook.com>
2021-10-22 06:05:29 -0500

    feat(kotlin): Some android utilities was added.

--- library/src/main/java/com/github/ushiosan23/android_utilities/extensions/AnyExt.kt
+++ library/src/main/java/com/github/ushiosan23/android_utilities/extensions/AnyExt.kt
@@ -8,3 +8,17 @@ package com.github.ushiosan23.android_utilities.extensions
  * @return Return the object class result
  */
 inline fun <T, reified V> T.resolveClass(): Class<V> = V::class.java
+
+/**
+ * Try to cast object to another type
+ *
+ * @param action Target action to execute if object was changed
+ * @return Return current object
+ */
+@Suppress("UNCHECKED_CAST")
+fun <T, V> T.tryCast(action: (item: V) -> V) = try {
+	action.invoke(this as V)
+	this
+} catch (ignored: Exception) {
+	this
+}
