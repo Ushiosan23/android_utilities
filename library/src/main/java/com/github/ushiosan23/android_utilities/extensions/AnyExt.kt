b65ea202ca4a14087b78073bd498d5e9fc03d775
Ushio Okasaki <haloleyendee@outlook.com>
2021-12-30 22:09:45 -0600

    refactor(kotlin): Change cast logic and more extensions was added

--- library/src/main/java/com/github/ushiosan23/android_utilities/extensions/AnyExt.kt
+++ library/src/main/java/com/github/ushiosan23/android_utilities/extensions/AnyExt.kt
@@ -1,6 +1,22 @@
 package com.github.ushiosan23.android_utilities.extensions
 
 /**
+ * Object string representation
+ *
+ * @return Special object string representation
+ */
+fun Any.toInfoString(): String =
+	"(@${Integer.toHexString(hashCode())}) ${javaClass.name}"
+
+/**
+ * Object string representation
+ *
+ * @return Special object short string representation
+ */
+fun Any.toShortInfoString(): String =
+	"(@${Integer.toHexString(hashCode())}) ${javaClass.simpleName}"
+
+/**
  * Return any object java class
  *
  * @param T Generic base object class
@@ -15,10 +31,22 @@ inline fun <T, reified V> T.resolveClass(): Class<V> = V::class.java
  * @param action Target action to execute if object was changed
  * @return Return current object
  */
-@Suppress("UNCHECKED_CAST")
-fun <T, V> T.tryCast(action: (item: V) -> V) = try {
-	action.invoke(this as V)
-	this
-} catch (ignored: Exception) {
-	this
+inline fun <reified V> Any.tryCast(action: (item: V) -> Unit): Any {
+	// Check types
+	if (this is V) action.invoke(this as V)
+	// Return current instance
+	return this
+}
+
+/**
+ * Try to cast object to another type
+ *
+ * @param action Target action to execute if object was changed
+ * @return Return current object
+ */
+inline fun <reified V> Any.tryCastContext(action: V.() -> Unit): Any {
+	// Check type
+	if (this is V) action.invoke(this as V)
+	// Return current instance
+	return this
 }
